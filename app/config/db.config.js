dab2bc0ec1b1ff0741bc4b3ce2300ca6d175379a
katarujr <katarujr@gmail.com>
2022-05-30 01:23:18 +0200

    config oki

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,5 +1,3 @@
-require('dotenv').config();
-
 module.exports = {
     HOST: "tag",
     USER: "postgres",
--- app/config/s3.config.js
+++ app/config/s3.config.js
@@ -1,9 +1,6 @@
-
-require('dotenv').config();
-
 module.exports = {
-    accessKeyId: process.env.KEY_ID,
-    secretAccessKey:process.env.KEY,
-    region:process.env.REGION,
-    bucket:process.env.BUCKET
+    accessKeyId: "ki",
+    secretAccessKey: "ks",
+    region: "rg",
+    bucket:"bk"
   };
