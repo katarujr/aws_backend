f47de424e542437a244d0c3f9fbe79719d759dbb
katarujr <katarujr@gmail.com>
2022-05-29 23:28:29 +0200

    modif config

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,20 @@
+const fs=require('fs')
+
+const get_host = ()=>{
+  fs.readFile('app/config/cfg','utf-8',(err,data)=>{
+    if(err){
+      console.log(err)
+      return "";
+    }
+    return data
+  })
+}
+
 module.exports = {
-    HOST: "",
-    USER: "",
-    PASSWORD: "",
-    DB: "",
+    HOST: get_host(),
+    USER: "postgres",
+    PASSWORD: "freshnugget",
+    DB: "awsdb",
     dialect: "postgres",
     pool: {
       max: 5,
