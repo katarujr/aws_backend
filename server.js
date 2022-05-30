51964cd4e258067ec57b833c5038a130d003bb2e
katarujr <katarujr@gmail.com>
2022-05-30 11:14:31 +0200

    fix cors

--- server.js
+++ server.js
@@ -3,10 +3,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
 
-var corsOptions = {
-  origin: "http://localhost:3000"
-};
-app.use(cors(corsOptions));
+app.use(cors());
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
 // parse requests of content-type - application/x-www-form-urlencoded
